33039f4c1ea97f6d067da27e85c7cc935e6d226f
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-04-23 06:29:11 +1100

    Add events listener and remove messages listener

--- src/lib/bolt/listeners/index.ts
+++ src/lib/bolt/listeners/index.ts
@@ -2,16 +2,16 @@ import type { App } from "@slack/bolt";
 import actions from "./actions";
 import assistant from "./assistant";
 import commands from "./commands";
+import events from "./events";
 import shortcuts from "./shortcuts";
-import messages from "./messages";
 import views from "./views";
 
 const registerListeners = (app: App) => {
   actions.register(app);
   assistant.register(app);
   commands.register(app);
+  events.register(app);
   shortcuts.register(app);
-  messages.register(app);
   views.register(app);
 };
 
