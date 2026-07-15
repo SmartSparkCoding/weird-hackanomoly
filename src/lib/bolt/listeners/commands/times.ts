70484f059c554c1f543630bbd04ffb48ba5e4297
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-07-15 20:48:52 +0100

    Change time format to 12-hour and include Jenny's time
    Updated time formatting to use 12-hour clock and added Jenny's time.

--- src/lib/bolt/listeners/commands/times.ts
+++ src/lib/bolt/listeners/commands/times.ts
@@ -11,19 +11,21 @@ const timesCommand: Middleware<SlackCommandMiddlewareArgs> = async ({
     new Intl.DateTimeFormat("en-GB", {
       hour: "2-digit",
       minute: "2-digit",
-      hour12: false,
+      hour12: true,
       timeZone: tz,
     }).format(new Date());
 
   const jacob = fmt("Europe/London");
   const abby = fmt("Asia/Kuwait");
   const techno = fmt("America/Chicago");
+  const jenny = fmt("America/New_York");
 
-  const message = `Heyo! Times are:
+  const message = `Heyo! (Daylight Savings Factored) Times are:
 
 • *Jacob* – ${jacob} (London, GMT/BST)
 • *Abby* – ${abby} (Kuwait, GMT+3)
 • *Techno* – ${techno} (CDT, GMT-5)
+• *Jenny* – ${jenny} (EDT, GMT-4)
 `;
 
   await respond({
